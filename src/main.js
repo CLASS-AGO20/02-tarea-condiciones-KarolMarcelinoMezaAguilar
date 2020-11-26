13842f4bfb2979f991f838ac15225e0f39f83219
Marcelino Meza <kmeza@ucol.mx>
2020-11-26 13:02:16 -0600

    added esPar function

--- src/main.js
+++ src/main.js
@@ -1,5 +1,72 @@
 export default class App {
- 
+
+    // Funcion que determine si un número es par
+    esPar(numero){
+        
+        let par;
+
+        if(numero % 2 === 0){
+            par = "true";
+        }else{
+            par = "false";
+        }
+
+        return par;
+
+    }
+
+    // Función que determina si un número es múltiplo de otro
+
+
+    // Función que calcule el salario de un trabajador
+
+
+    
+    // Función que convierta una calificación en letra
+
+
+    // Función que diga el número más cercano a 100
+
+
+    // Función que haga una operación 
+
+
+    // Función que haga rentas de películas
+
+
+    // Función que calcule el costo de boletos de teatro
+
+
+    // Función que determine si un número está dentro de un rango
+
 }
 
 let app = new App();
+
+// F1
+console.log(app.esPar(8));
+console.log(app.esPar(5));
+
+// F2
+
+
+// F3
+
+
+// F4
+
+
+// F5
+
+
+// F6
+
+
+// F7
+
+
+// F8
+
+
+// F9
+
